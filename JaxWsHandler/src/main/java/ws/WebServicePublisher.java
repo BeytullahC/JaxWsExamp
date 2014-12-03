a642b907c6c91a0afd044dd66e835c804edf885f
BeytullahC <beytullah.dakich@gmail.com>
2014-12-03 15:23:14 +0200

    add...

--- JaxWsHandler/src/main/java/ws/WebServicePublisher.java
+++ JaxWsHandler/src/main/java/ws/WebServicePublisher.java
@@ -5,6 +5,7 @@ import javax.xml.ws.Endpoint;
 public class WebServicePublisher{
  
 	public static void main(String[] args) {
+            
 	   Endpoint.publish("http://localhost:8878/webservice/validator", new WebServiceImpl());
     }
  
